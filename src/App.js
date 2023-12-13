4af2d468876238ad82d0d010f309585d8b94fe35
Omar Garcia <za19012225@zapopan.tecmm.edu.mx>
2023-12-12 20:58:45 -0600

    Creando funciones para el componente

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import Boton from './component/Boton';
 
 // Definimos como queremos que se llame la IMG que vamos a IMPORTAR
 import logo from './img/logo.svg';
 
 function App() {
+  const manejarClic = () => {
+    console.log('clic');
+  };
+
+  const reiniciarContador = () => {
+    console.log('Reiniciar');
+  };
+
   return (
     <div className='App'>
       <div className='logo-contenedor'>
@@ -16,7 +25,16 @@ function App() {
       </div>
 
       <div className='contenedor-principal'>
-
+        <Boton
+          texto='Clic'
+          btnClic={true}
+          manejarClic={manejarClic}
+        />
+        <Boton
+          texto='Reiniciar'
+          btnClic={false}
+          manejarClic={reiniciarContador}
+        />
       </div>
     </div>
   );
