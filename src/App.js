5cf2f8ee4e8d8d74da336d0a984abd81bd882a38
Omar Garcia <za19012225@zapopan.tecmm.edu.mx>
2023-12-12 20:31:09 -0600

    Personalizando App y añadiendo CSS

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
 
+// Definimos como queremos que se llame la IMG que vamos a IMPORTAR
+import logo from './img/logo.svg';
+
 function App() {
   return (
-    <div className="App">
-
+    <div className='App'>
+      <div className='logo-contenedor'>
+        <img 
+          className='logo'
+          // Utilizamos el import de LOGO
+          src={logo}
+          alt='Logo'
+        />
+      </div>
     </div>
   );
 }
